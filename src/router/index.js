e580b045df91a7b011205b4b774dd1cc85daf745
Mrqinforce <1293340422@qq.com>
2020-05-17 10:03:24 +0800

    歌手页面以及详情页中的个人信息和热门歌曲

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,10 @@ export default new Router({
     {
       path: "/user",
       component: () => import("../components/user.vue")
+    },
+    {
+      path: "/singer",
+      component: () => import("../components/singer.vue")
     }
   ]
 });
